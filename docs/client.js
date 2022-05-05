f8ab1b4416c0ba4f21c6ce88bce7ae08e398c4a4
7fi <carterwilliamanderson@gmail.com>
2022-05-05 16:34:57 -0700

    Fleshing out Portfolio

--- docs/client.js
+++ docs/client.js
@@ -1,9 +1,20 @@
 const modeToggle = document.getElementById('modeToggle');
 let lightMode = false;
 
+function load(){
+    let myBool = (decodeURIComponent(document.cookie).split('=')[1] === 'true');
+    console.log("Lightmode is wrong:", myBool != lightMode)
+    if(myBool != lightMode){
+        switchMode();
+    }
+}
 
 // Toggle between light and dark mode
 modeToggle.addEventListener('click', () => {
+    switchMode();
+})
+
+function switchMode(){
     if(!lightMode){
         modeToggle.children[0].classList.replace('gg-sun','gg-moon');
         document.documentElement.style.setProperty('--dark','#eaeaea');
@@ -11,6 +22,7 @@ modeToggle.addEventListener('click', () => {
         document.documentElement.style.setProperty('--med','#cfcfcf');
         document.documentElement.style.setProperty('--medLight','#d1d1d1');
         document.documentElement.style.setProperty('--light','#b3b3b3');
+        document.documentElement.style.setProperty('--veryLight','#222');
         document.documentElement.style.setProperty('--highlight','#000');
         document.documentElement.style.setProperty('--highlight1','#12A5B1');
     }else{
@@ -20,8 +32,11 @@ modeToggle.addEventListener('click', () => {
         document.documentElement.style.setProperty('--med','#555');
         document.documentElement.style.setProperty('--medLight','#666');
         document.documentElement.style.setProperty('--light','#777');
+        document.documentElement.style.setProperty('--veryLight','#ccc');
         document.documentElement.style.setProperty('--highlight','#fff');
         document.documentElement.style.setProperty('--highlight1','#222');
     }
     lightMode = !lightMode;
-})
+    document.cookie = ("lightMode=" + lightMode + "; path=/");
+    console.log(lightMode)
+}
