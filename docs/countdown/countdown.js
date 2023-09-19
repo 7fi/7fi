947d9d26f32d7da749ab341f97763df3c765353c
7fi <carterwilliamanderson@gmail.com>
2023-09-18 23:50:16 -0700

    minor udpates

--- docs/countdown/countdown.js
+++ docs/countdown/countdown.js
@@ -2,11 +2,18 @@ const dayNum = document.getElementById('dayNum')
 const hourNum = document.getElementById('hourNum')
 const minNum = document.getElementById('minNum')
 const secNum = document.getElementById('secNum')
+const perNum = document.getElementById('percentNum')
 
-const countdown = () => {
+const countdown = (date) => {
   const countDate = new Date('November 23, 2023 8:00:00').getTime()
+  const startDate = new Date('August 22, 2023 8:20:00').getTime()
   const now = new Date().getTime()
   const gap = countDate - now
+  const soFar = now - startDate
+  const percent = Math.round((1 - gap / (countDate - startDate)) * 100)
+  console.log(percent)
+  // console.log(document.getElementById('progressBar'))
+  document.getElementById('progressBar').style.width = percent + '%'
 
   const second = 1000
   const minute = second * 60
@@ -18,10 +25,13 @@ const countdown = () => {
   const textMinute = Math.floor((gap % hour) / minute)
   const textSecond = Math.floor((gap % minute) / second)
 
+  // return {day: textDay, h}
+
   dayNum.innerText = textDay
   hourNum.innerText = textHour
   minNum.innerText = textMinute
   secNum.innerText = textSecond
+  perNum.innerText = percent + '%'
 
   document.title = ('0' + textDay).slice(-2) + ':' + ('0' + textHour).slice(-2) + ':' + ('0' + textMinute).slice(-2) + ':' + ('0' + textSecond).slice(-2)
 }
